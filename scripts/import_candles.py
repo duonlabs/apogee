dfbab4decff0b8d681f3ace732b3636fcc4e02c1
Théo Boyer <boyert@protonmail.com>
2025-02-22 09:21:30 +0100

    Import without saving local files

--- scripts/import_candles.py
+++ scripts/import_candles.py
@@ -1,15 +1,39 @@
+import io
+import requests
 import argparse
+import huggingface_hub
 import pandas as pd
 import numpy as np
 
+from typing import Union
 from pathlib import Path
+
 from apogee.data.sourcing.binance import get_pair_candles
 
-def save_pair_candles(pair: str, n_workers: int = 10, dataset_path: Path = Path("dataset")):
-    provider_path = dataset_path / "binance"
-    provider_path.mkdir(parents=True, exist_ok=True)
-    metadata_path = dataset_path / "metadata.csv"
-    metadata = pd.read_csv(metadata_path, index_col="key")
+def get_file_from_repo(repo_id: str, filepath: Union[str, Path]) -> bytes:
+    response = requests.get(f"https://huggingface.co/datasets/{repo_id}/raw/main/{filepath}")
+    response.raise_for_status()
+    return response.content
+
+def get_metadata(repo_id: str) -> pd.DataFrame:
+    try:
+        content = get_file_from_repo(repo_id, "metadata.csv").decode(encoding="utf-8")
+    except requests.HTTPError as e:
+        if e.response.status_code == 404:
+            # Create a new metadata file
+            content = "key,start,end,freq\n"
+            huggingface_hub.create_commit(
+                repo_id=repo_id,
+                operations=[huggingface_hub.CommitOperationAdd("metadata.csv", content.encode("utf-8"))],
+                commit_message="Initialize metadata.csv",
+                repo_type="dataset",
+            )
+        raise e
+    return pd.read_csv(io.StringIO(content), index_col="key")
+
+def save_pair_candles(pair: str, repo_id: str = "duonlabs/apogee", n_workers: int = 10):
+    metadata = get_metadata(repo_id)
+    is_new = pair not in metadata.index
     df = get_pair_candles(pair, n_workers)
     timestamps = df["timestamp"].values
     start = timestamps[0]
@@ -28,14 +52,25 @@ def save_pair_candles(pair: str, n_workers: int = 10, dataset_path: Path = Path(
         "end": end,
         "freq": 60,
     }
-    metadata.to_csv(metadata_path)
-    np.save(provider_path / f"{pair}.npy", buffer)
+    npy_buffer = io.BytesIO()
+    np.save(npy_buffer, buffer)
+    provider, pair = key.split(".")
+    huggingface_hub.create_commit(
+        repo_id=repo_id,
+        operations=[
+            huggingface_hub.CommitOperationAdd(f"{provider}/{pair}.npy", npy_buffer.getvalue()),
+            huggingface_hub.CommitOperationAdd("metadata.csv", metadata.to_csv().encode("utf-8")),
+        ],
+        commit_message=f"Imported {pair}" if is_new else f"Updated {pair}",
+        repo_type="dataset",
+    )
 
 # Example usage:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Download and save pair candles data.")
     parser.add_argument("pair", type=str, help="The trading pair to download data for (e.g., BTCUSDT).")
+    parser.add_argument("--repo_id", type=str, default="duonlabs/apogee", help="The Hugging Face Hub repository ID to save the data.")
     parser.add_argument("--n_workers", type=int, default=10, help="Number of worker threads to use for downloading data.")
     args = parser.parse_args()
-    
-    save_pair_candles(args.pair, args.n_workers)
+    huggingface_hub.login()
+    save_pair_candles(args.pair, repo_id=args.repo_id, n_workers=args.n_workers)
