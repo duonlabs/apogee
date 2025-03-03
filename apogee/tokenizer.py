dea1716cdea068ac665ca4c53742e872dd140fe9
Théo Boyer <boyert@protonmail.com>
2025-03-03 15:37:45 +0100

    Coment the tokenizer

--- apogee/tokenizer.py
+++ apogee/tokenizer.py
@@ -18,22 +18,22 @@ class Tokenizer:
 
     def encode(self, candles: Union[np.array, torch.Tensor]) -> torch.Tensor:
         """Tokenize candles into tokens."""
-        if isinstance(candles, np.ndarray):
+        if isinstance(candles, np.ndarray): # Wrap into a tensor
             candles = torch.tensor(candles)
-        if sys.byteorder == 'little':
+        if sys.byteorder == 'little':# On little-endian systems, we need to byteswap the data so that msb is first
             candles.untyped_storage().byteswap(torch.float32)
-        buffer = candles.view(torch.uint8)
-        buffer = buffer.view(-1).to(torch.uint16)
+        buffer = candles.view(torch.uint8) # Interpret the data as bytes ("tokenization" step)
+        buffer = buffer.view(-1).to(torch.uint16) # Flatten the data and convert to uint16 because otherwise <BOS> will overflow
         buffer = torch.cat([torch.tensor([256], dtype=torch.uint16), buffer]) # Prepend <BOS> (Begin of Series) token
         return buffer
     
     def decode(self, tokens: torch.Tensor) -> torch.Tensor:
         """Decode tokens into candles."""
         tokens = tokens.long()
-        candles_tokens = tokens[..., 1:]
-        candles_tokens = candles_tokens.to(torch.uint8).view(*tokens.shape[:-1], -1, self.tokens_per_candle)
-        candles_tokens = candles_tokens.view(torch.float32)
-        if sys.byteorder == 'little':
+        candles_tokens = tokens[..., 1:] # Remove <BOS> token
+        candles_tokens = candles_tokens.to(torch.uint8).view(*tokens.shape[:-1], -1, self.tokens_per_candle) # Convert back to uint8 and reshape
+        candles_tokens = candles_tokens.view(torch.float32) # Interpret the data as floats
+        if sys.byteorder == 'little': # On little-endian systems, we need to byteswap the data back
             # candles_tokens.untyped_storage().byteswap(torch.float32) # <-- This segfaults for some reason
-            candles_tokens = candles_tokens.view(torch.uint8).view(*candles_tokens.shape, 4).flip(-1).view(torch.float32).squeeze(-1)
+            candles_tokens = candles_tokens.view(torch.uint8).view(*candles_tokens.shape, 4).flip(-1).view(torch.float32).squeeze(-1)# Workaround
         return candles_tokens
